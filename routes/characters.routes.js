62acb58167cdea8dbd4a0c1871d12de71edec927
Ralf Bonnick <ralfbonnick@gmx.net>
2022-11-02 17:01:54 +0100

    finish update

--- routes/characters.routes.js
+++ routes/characters.routes.js
@@ -50,6 +50,42 @@ router.post('/new',async (req, res) => {
     {console.log("error")}
   })
 
+// create get for update
+
+router.get("/characters/:id/edit", (req, res, next) => {
+    axios.get(`https://ih-crud-api.herokuapp.com/characters/${req.params.id}`)
+    //console.log("XXXXXXX", req.params.id)
+    .then(responseFromAPI => {
+        // console.log("details: ", responseFromAPI.data)
+        res.render("characters/edit-character", { character: responseFromAPI.data });
+    })
+    .catch(err => console.error(err))
+    });
+
+// create post update
+
+router.post('/characters/:id/edit',async (req, res) => {
+    console.log(req.params.id)
+    try
+    {
+        const update = await axios({
+            method: 'put',
+            url: 'https://ih-crud-api.herokuapp.com/characters/'+ req.params.id,
+            data: {
+                name: req.body.name,
+                occupation: req.body.occupation,
+                weapon:  req.body.weapon,
+                dept: req.body.dept
+            }    
+            });
+       //console.log(update);
+
+        const responseFromAPI = await axios.get("https://ih-crud-api.herokuapp.com/characters")
+        res.render("characters/list-characters", { characters: responseFromAPI.data });
+    }
+    catch(error)
+    {console.log(error)}
+  })
 
 
 module.exports = router;
